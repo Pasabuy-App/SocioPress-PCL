cac2b700bbbced6188005f0d90ecfb328ff4d42a
RusselJay27 <ponferrada.russel27@gmail.com>
2020-07-28 17:30:25 +0800

    Added H_Feeds Class - Home Feeds.

---
+++ Home/H_Feeds.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using SocioPress.Profile.Struck;
+using System.Net.Http;
+
+namespace SocioPress.Profile
+{
+    public class H_Feeds
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Home Feeds Class.
+        /// </summary>
+        private static H_Feeds instance;
+        public static H_Feeds Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new H_Feeds();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication for our Backend.
+        /// </summary>
+        HttpClient client;
+        public H_Feeds()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Method
+        public async void GetData(string wp_id, string session_key, Action<bool, string> callback)
+        {
+            string getRequest = "?";
+            getRequest += "wpid" + wp_id;
+            getRequest += "&snky" + session_key;
+
+            var response = await client.GetAsync(BaseClass.BaseDomainUrl + "/datavice/api/v1/home/feed" + getRequest);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}
