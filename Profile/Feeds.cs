e6cbaf9bd40ae5c8815679077f8c1f3453dd1c9f
RusselJay27 <ponferrada.russel27@gmail.com>
2020-07-28 17:25:54 +0800

    Added Feeds Class.

---
+++ Profile/Feeds.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using SocioPress.Profile.Struck;
+using System.Net.Http;
+
+namespace SocioPress.Profile
+{
+    public class Feeds
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Profile Feeds Class.
+        /// </summary>
+        private static Feeds instance;
+        public static Feeds Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Feeds();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication for our Backend.
+        /// </summary>
+        HttpClient client;
+        public Feeds()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Method
+        public async void GetData(string wp_id, string session_key, Action<bool, string> callback)
+        {
+            string getRequest = "?";
+            getRequest += "wpid" + wp_id;
+            getRequest += "&snky" + session_key;
+
+            var response = await client.GetAsync(BaseClass.BaseDomainUrl + "/sociopress/api/v1/user/feeds" + getRequest);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}
