c7cd6650b7d65fe4fecdc999b380ea2fd48d74e5
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-19 21:52:57 +0800

    Added Reviews.

---
+++ Reviews/Review.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using SocioPress.Profile.Struck;
+
+namespace SocioPress.Reviews
+{
+    public class Review
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Insert Reviews Class.
+        /// </summary>
+        private static Review instance;
+        public static Review Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Review();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public Review()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Methods
+        public async void Insert(string wp_id, string session_key, string comment, string mover_id, string rating, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            dict.Add("msg", comment);
+            dict.Add("rid", mover_id);
+            dict.Add("rat", rating);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/sociopress/v1/reviews/insert", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}
