f3e8ae1b28e0c114ef434ec3033330956cf01105
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-19 22:03:43 +0800

    Added Get total transaction of user.

---
+++ Transaction/GetTotal.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Net.Http;
+using SocioPress.Profile.Struck;
+
+namespace SocioPress.Transaction
+{
+    public class GetTotal
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Get Total Transaction of User Class.
+        /// </summary>
+        private static GetTotal instance;
+        public static GetTotal Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new GetTotal();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public GetTotal()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Methods
+        public async void Transaction(string wp_id, string session_key, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/sociopress/v1/transactions/user/list/total", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}
