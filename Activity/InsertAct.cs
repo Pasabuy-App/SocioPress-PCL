911e2eadf49c2ce1989b50ca55902d85ec222be8
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-24 20:50:14 +0800

    Added Insert Activity.

---
+++ Activity/InsertAct.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using SocioPress.Profile.Struck;
+using System.Net.Http;
+
+namespace SocioPress.Activity
+{
+    public class InsertAct
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Insert Activity Class.
+        /// </summary>
+        private static InsertAct instance;
+        public static InsertAct Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new InsertAct();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public InsertAct()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Methods
+        public async void Insert(string wp_id, string session_key, string stid, string title, string info, string icon, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            if (stid != "")
+            {
+                dict.Add("stid", stid);
+            }
+            dict.Add("title", title);
+            dict.Add("info", info);
+            dict.Add("icon", icon);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/sociopress/v1/activity/insert", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}
