a2c7932f07082649c47d1327830110501c13d257
RusselJay27 <ponferrada.russel27@gmail.com>
2020-08-24 20:53:36 +0800

    Added Open Activity.

---
+++ Activity/OpenAct.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using SocioPress.Profile.Struck;
+using System.Net.Http;
+
+namespace SocioPress.Activity
+{
+    public class OpenAct
+    {
+        #region Fields
+        /// <summary>
+        /// Instance of Open Activity Class.
+        /// </summary>
+        private static OpenAct instance;
+        public static OpenAct Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new OpenAct();
+                return instance;
+            }
+        }
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Web service for communication to our Backend.
+        /// </summary>
+        HttpClient client;
+        public OpenAct()
+        {
+            client = new HttpClient();
+        }
+        #endregion
+        #region Methods
+        public async void Open(string wp_id, string session_key, string atid, string stid, Action<bool, string> callback)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("wpid", wp_id);
+            dict.Add("snky", session_key);
+            dict.Add("atid", atid);
+            dict.Add("stid", stid);
+            var content = new FormUrlEncodedContent(dict);
+
+            var response = await client.PostAsync(BaseClass.BaseDomainUrl + "/sociopress/v1/activity/select", content);
+            response.EnsureSuccessStatusCode();
+
+            if (response.IsSuccessStatusCode)
+            {
+                string result = await response.Content.ReadAsStringAsync();
+                Token token = JsonConvert.DeserializeObject<Token>(result);
+
+                bool success = token.status == "success" ? true : false;
+                string data = token.status == "success" ? result : token.message;
+                callback(success, data);
+            }
+            else
+            {
+                callback(false, "Network Error! Check your connection.");
+            }
+        }
+        #endregion
+    }
+}
